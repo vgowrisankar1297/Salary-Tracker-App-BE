a3951ea6e200e235819d80389680b46a1a55e361
vgowrisankar1297 <vgowrisankar1297@gmail.com>
2026-06-11 20:52:00 +0530

    Update DashboardController.java

--- src/main/java/com/salarytracker/backend/controller/DashboardController.java
+++ src/main/java/com/salarytracker/backend/controller/DashboardController.java
@@ -7,11 +7,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/dashboard")
-@CrossOrigin("*")
 public class DashboardController {
 
     private final DashboardService service;
